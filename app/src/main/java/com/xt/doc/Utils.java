a07cab6b5b9513d44aed1c37da8d9c9abafdad78
xuti <15207127876@163.com>
2024-06-27 13:30:47 +0800

    系统日志

---
+++ app/src/main/java/com/xt/doc/Utils.java
@@ -0,0 +1,24 @@
+package com.xt.doc;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.View;
+
+public class Utils {
+    public static int getMeasuredWidth(View view) {
+        int widthMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        int heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        view.measure(widthMeasureSpec, heightMeasureSpec);
+        return view.getMeasuredWidth();
+    }
+
+    public static BitmapFactory.Options getImageWidthHeight(Context context, int resId) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        //返回的bitmap为空，options.outHeight已计算
+        options.inJustDecodeBounds = true;
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), resId, options); // 此时返回的bitmap为null
+        //options.outHeight为原始图片宽高
+        return options;
+    }
+}
