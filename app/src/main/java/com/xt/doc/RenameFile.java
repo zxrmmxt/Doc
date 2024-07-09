11b78917687b754b5c8df3046ba336b093f6991d
xuti <15207127876@163.com>
2024-07-09 19:01:49 +0800

    java

--- app/src/main/java/com/xt/doc/RenameFile.java
+++ app/src/main/java/com/xt/doc/RenameFile.java
@@ -1,64 +1,37 @@
 package com.xt.doc;
 
-import androidx.annotation.NonNull;
-
 import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class RenameFile {
     private static final String TAG = RenameFile.class.getSimpleName();
     // 获取当前类所在的路径
     private static final String PROJECT_PATH = System.getProperty("user.dir");
+    private static final String APK_DIR = "D:\\WORK\\ASProject\\Chery\\dev_t28\\LTNavigationService\\app\\build\\outputs\\apk\\e0x_btech_8295Inner\\debug";
 
     public static void main(String[] args) {
         new Thread(() -> {
 
             // 输出路径
-            System.out.println("当前项目的根路径是: " + PROJECT_PATH);
-            rename("/drawable/cross/day/hud_sou","/drawable/cross/day/lt_image_cross_direction");
-            rename("/drawable/cross/night/hud_sou","/drawable/cross/night/lt_image_cross_direction");
-            rename("/drawable/tbt/day/hud_sou","/drawable/tbt/day/lt_image_hud_sou");
-            rename("/drawable/tbt/night/hud_sou","/drawable/tbt/night/lt_image_hud_sou");
+//            System.out.println("当前项目的根路径是: " + PROJECT_PATH);
+//            renameDrawable();
+//            System.out.println(Utils.buildLogContent("apk安装指令:[adb install -r -d {?}]", Utils.getFilePath(new File(APK_DIR), ".apk")));
+            Utils.exeAdb(Utils.buildStr("adb install -r -d {?}", Utils.getFilePath(new File(APK_DIR), ".apk")));
+
         }).start();
+    }
+
+    private static void renameDrawable() {
+        rename("/drawable/cross/day/hud_sou", "/drawable/cross/day/lt_image_cross_direction");
+        rename("/drawable/cross/night/hud_sou", "/drawable/cross/night/lt_image_cross_direction");
+        rename("/drawable/tbt/day/hud_sou", "/drawable/tbt/day/lt_image_hud_sou");
+        rename("/drawable/tbt/night/hud_sou", "/drawable/tbt/night/lt_image_hud_sou");
     }
 
     private static void rename(String oldName, String newName) {
         for (int i = 0; i < 72; i++) {
-            if (!renameFile(PROJECT_PATH + oldName + i + ".png", PROJECT_PATH + newName + i + ".png")) {
-                System.out.println(buildLogContent("失败序号:{?}", i));
+            if (!Utils.renameFile(PROJECT_PATH + oldName + i + ".png", PROJECT_PATH + newName + i + ".png")) {
+                System.out.println(Utils.buildStr("失败序号:{?}", i));
             }
         }
-    }
-
-    public static boolean renameFile(@NonNull String oriFilePath, @NonNull String destFilePath) {
-        File srcFile = new File(oriFilePath);
-        if (!srcFile.exists()) {
-            return false;
-        }
-
-        File dest = new File(destFilePath);
-        dest.getParentFile().mkdirs();
-        return srcFile.renameTo(dest);
-    }
-
-    private static String buildLogContent(String msg, Object... params) {
-        if (msg == null) {
-            return "";
-        }
-        if (params == null) {
-            return msg;
-        }
-        StringBuilder bf = new StringBuilder();
-        String[] msgArray = msg.split("\\{\\?\\}");
-        int minLen = Math.min(msgArray.length, params.length);
-        for (int i = 0; i < minLen; i++) {
-            Object param = params[i];
-            bf.append(msgArray[i]).append(param);
-        }
-        for (int i = minLen; i < msgArray.length; i++) {
-            bf.append(msgArray[i]);
-        }
-        return bf.toString();
     }
 }
--- app/src/main/java/com/xt/doc/Utils.java
+++ app/src/main/java/com/xt/doc/Utils.java
@@ -5,6 +5,12 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 public class Utils {
     public static int getMeasuredWidth(View view) {
         int widthMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
@@ -20,5 +26,109 @@ public class Utils {
         Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), resId, options); // 此时返回的bitmap为null
         //options.outHeight为原始图片宽高
         return options;
+    }
+
+    public static boolean renameFile(@NonNull String oriFilePath, @NonNull String destFilePath) {
+        File srcFile = new File(oriFilePath);
+        if (!srcFile.exists()) {
+            return false;
+        }
+
+        File dest = new File(destFilePath);
+        dest.getParentFile().mkdirs();
+        return srcFile.renameTo(dest);
+    }
+
+    public static String buildStr(String msg, Object... params) {
+        if (msg == null) {
+            return "";
+        }
+        if (params == null) {
+            return msg;
+        }
+        StringBuilder bf = new StringBuilder();
+        String[] msgArray = msg.split("\\{\\?\\}");
+        int minLen = Math.min(msgArray.length, params.length);
+        for (int i = 0; i < minLen; i++) {
+            Object param = params[i];
+            bf.append(msgArray[i]).append(param);
+        }
+        for (int i = minLen; i < msgArray.length; i++) {
+            bf.append(msgArray[i]);
+        }
+        return bf.toString();
+    }
+
+    public static void listFiles(File directory) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    listFiles(file);
+                } else {
+                    System.out.println(file.getAbsolutePath());
+                }
+            }
+        }
+    }
+
+    /**
+     * 搜索获取文件绝对路径
+     *
+     * @param directory 指定目录
+     * @param matchStr  匹配词
+     * @return 文件绝对路径
+     */
+    public static String getFilePath(File directory, String matchStr) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    listFiles(file);
+                } else {
+                    if (file.getName().contains(matchStr)) {
+                        return file.getAbsolutePath();
+                    }
+                }
+            }
+        }
+        return "";
+    }
+
+    public static void exeAdb(String command) {
+        try {
+            System.out.println(command);
+            Process process = Runtime.getRuntime().exec(command);
+
+            // 读取并打印adb命令的输出
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // 等待命令执行完成
+            process.waitFor();
+            System.out.println("Command executed successfully.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void copy() {
+        // 获取系统剪贴板
+//        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+//
+//        // 要复制的文本
+//        String textToCopy = "这是要复制到剪贴板的文本";
+//
+//        // 将文本转换为Transferable对象
+//        StringSelection stringSelection = new StringSelection(textToCopy);
+//
+//        // 将文本放入剪贴板
+//        clipboard.setContents(stringSelection, null);
+//
+//        System.out.println("文本已复制到剪贴板。");
     }
 }
