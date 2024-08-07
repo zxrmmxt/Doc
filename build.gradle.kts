85441465e5349567baaed0588e833e97dff9b153
xuti <15207127876@163.com>
2024-08-07 10:56:00 +0800

    面试

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.1-Android：最全面&详细的性能优化攻略（含内存优化、内存泄漏、绘制优化、布局优化、图片优化、APK优化、多线程优化、列表优化等）.2" apply false
+    id("org.jetbrains.kotlin.android") version "1-Android：最全面&详细的性能优化攻略（含内存优化、内存泄漏、绘制优化、布局优化、图片优化、APK优化、多线程优化、列表优化等）.9.0" apply false
 }
